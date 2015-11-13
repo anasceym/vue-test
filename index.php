40beb8df5f18e0ef3bd42540288373993e525c94
anasceym <anasfirdauss@gmail.com>
2015-11-13 22:38:20 +0800

    Rebasing project

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="css/custom.css"/>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1>Hello World!</h1>
+				</div>
+			</div>
+		</div>
+	<script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+	</body>
+</html>
